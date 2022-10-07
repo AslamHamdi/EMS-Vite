b3b2b909260848ef35a08ff5e2375f0c461452da
AslamHamdi <wokle08@gmail.com>
2022-10-08 01:29:15 +0800

    update

--- controllers/server.js
+++ controllers/server.js
@@ -9,8 +9,6 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(fileUpload());
 
-app.use('/userAssets', express.static(path.join(__dirname, '../app_data/user_files/')));
-
 app.use('/api/v1', require('./apiController'))
 
 app.listen(PORT, () => console.log(`start listening on port : ${PORT}`));
