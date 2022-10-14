10533109346736f34680c23e7557c3767bce6977
AslamHamdi <aslam.hamdi@growth-rts.com>
2022-10-14 10:00:03 +0800

    test

--- src/components/employee/Employee.vue
+++ src/components/employee/Employee.vue
@@ -12,6 +12,7 @@
                     </button>
                 </div>
             </div>
+            <!-- test -->
             <div class="relative mb-2" style="width: 97%; height: 5%;">
                 <input type="text" id="voice-search" v-model="searchText" @change="onSearchChange()"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-2xl block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
